1265dda8b2006d61bec6487c01ed590c557da97f
ilus1 <ilusdasilva@gmail.com>
2023-11-15 13:01:27 -0300

    fix: Corrigindo estrutura e css da pagina

--- src/pages/ShoppingList/index.tsx
+++ src/pages/ShoppingList/index.tsx
@@ -1,14 +1,13 @@
 import Footer from '../../components/Footer'
 import Topbar from '../../components/Topbar'
-import { Container } from './styles'
 
 
 function ShoppingList() {
   return (
-    <Container>
+    <>
       <Topbar />
       <Footer />
-    </Container>
+    </>
   )
 }
 
