a37234c19c732ab59bd67d4a9f97fe4887ff5402
ilus1 <ilusdasilva@gmail.com>
2023-11-16 16:12:21 -0300

    feat: Adicionando configuracao do framer-motion

---
+++ src/utils/animationUtils.ts
@@ -0,0 +1,18 @@
+export const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2
+    }
+  }),
+  closed: {
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40
+    }
+  }
+};
